b52411f22d6e13d7d2e838f46b9fce0cd510ed9f
Reinaldo G. P. Neto <reinaldogpn@outlook.com>
2024-06-22 17:47:51 -0300

    Project reset

---
+++ SalesWebMvc/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SalesWebMvc.Data;
+namespace SalesWebMvc
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<SalesWebMvcContext>(
+                options => options.UseMySql(builder.Configuration.GetConnectionString("SalesWebMvcContext"),
+                new MySqlServerVersion(new Version("8.0.2")),
+                builder => builder.MigrationsAssembly("SalesWebMvc")));
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
