588a941fe95f570cc2a7dc62baa5212ca0b15a30
Reinaldo G. P. Neto <reinaldogpn@outlook.com>
2024-06-22 21:04:24 -0300

    SellerFormViewModel and Department select component

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -17,6 +17,7 @@ namespace SalesWebMvc
             // Register created services
             builder.Services.AddScoped<SeedingService>();
             builder.Services.AddScoped<SellerService>();
+            builder.Services.AddScoped<DepartmentService>();
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
